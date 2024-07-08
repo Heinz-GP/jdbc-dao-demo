3a26fe5044cf9a5e6dec396ab4fd194ed66a3349
Heinz-GP <heinzgprediger@gmail.com>
2024-07-08 16:47:46 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entitites.Department;
+import model.entitites.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department dep = new Department(1, "Books");
-		
 		System.out.println(dep);
 		
-	}
+		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.00, dep);
+		System.out.println(seller);
+		}
 }
