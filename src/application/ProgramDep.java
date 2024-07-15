6f4ecb58ed8f6427cf1a7ff2095059c597aebb94
Heinz-GP <heinzgprediger@gmail.com>
2024-07-15 16:35:56 -0300

    Department - Insert, Update, Delete Implemetation

--- src/application/ProgramDep.java
+++ src/application/ProgramDep.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -10,22 +11,56 @@ public class ProgramDep {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
 		DepartmentDao depDao = DaoFactory.createDepartmentDao();
 		
 		System.out.println("====TEST 1: Find by Id ===");
 		Department dep = depDao.findById(1);
 		System.out.println(dep);
-		System.out.println("==== END TEST 1 ====");
+		System.out.println();
 
 		
-		System.out.println("====TEST 2: Find by All ===");
+		System.out.println("====TEST 2: Find All ===");
 		
 		List<Department> list = depDao.findAll();
 		for(Department obj : list) {
 			System.out.println(obj);
 		}
+		System.out.println();
 		
-		System.out.println("==== END TEST 2 ====");
+		
+		
+		System.out.println("====TEST 3: Insert ===");
+		System.out.print("Name of the department: ");
+		String name = sc.nextLine();
+		dep.setName(name);
+		depDao.insert(dep);	
+		System.out.println(dep);
+		System.out.println();
+		
+		
+		
+		System.out.println("====TEST 4: Update ===");
+		System.out.print("Department name update: ");
+		String depNewName = sc.nextLine();
+		System.out.print("Department id which name gonna be update: ");
+		int id = sc.nextInt();
+		dep = depDao.findById(id);
+		dep.setName(depNewName);
+		depDao.update(dep);
+		System.out.println(dep);
+		
+		
+		System.out.println("====TEST 4: Delete ===");
+		System.out.print("Department for delete: ");
+		int id2 = sc.nextInt();
+		depDao.deleteById(id2);
+		System.out.println("Department Delete!!!");
+		
+		
+		
+		sc.close();
 	}
 
 }
